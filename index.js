c387675b800cb6f8724cfbfe60b74c4123d6503a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-07-02 16:12:08 +0600

    new codeadded

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 5000;
 // this is midlewiere
 app.use(cors());
 app.use(express.json());
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // This is from database
 
